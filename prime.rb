ae6fd1a8c258915c809691ac84ec3d4cf3d190b4
Min Hee  Park <minheekangg@gmail.com>
2018-10-03 00:19:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  if num > 2
+    (2...num).each do |n|
+        if num % n == 0
+          return false
+        end
+      end
+    return true
+  end
+if num <=1
+  return false
+end
+if num == 2
+  return true
+end
+end
